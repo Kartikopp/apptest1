9d83fc9bb1b9cfa5957c4d93848f5e0b6075c96a
Kartikopp <141641423+Kartikopp@users.noreply.github.com>
2026-03-28 12:00:45 +0530

    Delete final app directory

--- final app/src/pages/CourseDetailPage.tsx
+++
@@ -1,6 +0,0 @@
-
-import { useState } from "react";
-import { useQuery, useMutation } from "convex/react";
-import { api } from "../../convex/_generated/api";
-import { Page } from "../App";
-import { Id
